cd230008eb7818b16c59dc9b11c50d05938f05c2
Pedro Tedim <1091234@isep.ipp.pt>
2018-12-17 12:00:35 +0000

    make it

--- SIC_E/server.js
+++ SIC_E/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 1337;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
