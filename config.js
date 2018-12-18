a27ab91ea13421aa212c410d2c4670179e16b86e
Pedro Tedim <1091234@isep.ipp.pt>
2018-12-18 01:26:11 +0000

    make it better

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    'database': 'mongodb://arqsi:sic1234567@ds024748.mlab.com:24748/sic_e',
-
-        // SiC connection
-    "urlSiC": "sicarqsi.azurewebsites.net",
-    };
-
---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+let ItemSchema = new mongoose.Schema ({
+    productId : Number,
+    name: String,
+    material : String,
+    finish : Number,
+    width : Number,
+    height : Number,
+    depth : Number,
+    area : Number,
+    children : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item'}]
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
